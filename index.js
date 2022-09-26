6caa8e7491495025c00cdcff0f276036ffb370f8
Erva <erva.ergul98@gmail.com>
2022-09-26 13:43:55 +0300

    small changes

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ function toggleTheme() {
   console.log("a")
   if (document.body.classList.contains("darkTheme")) {
     document.body.classList.replace("darkTheme", "lightTheme")
-    document.getElementById("toggleThemeImage").setAttribute("src", "moon.png")
+    document.getElementById("toggleThemeImageSidebar").setAttribute("src", "moon.png")
+    document.getElementById("toggleThemeImageTopbar").setAttribute("src", "moon.png")
   } else {
     document.body.classList.replace("lightTheme", "darkTheme")
-    document.getElementById("toggleThemeImage").setAttribute("src", "sun.png")
+    document.getElementById("toggleThemeImageSidebar").setAttribute("src", "sun.png")
+    document.getElementById("toggleThemeImageTopbar").setAttribute("src", "sun.png")
   }
 }
